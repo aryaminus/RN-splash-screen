9ed8049b5bc55b8f5d0799cc272159959f86c96f
Sunim Acharya <sunim.54@gmail.com>
2017-10-15 22:48:07 +0545

    Follow Code.txt

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-
+import SplashScreen from 'react-native-splash-screen';
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
